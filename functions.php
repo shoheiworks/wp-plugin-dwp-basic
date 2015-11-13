31661685accbc824f989dcb4f9f399818233e7d5
Shohei <shoheiworks@gmail.com>
2015-11-13 11:32:23 +0900

    add functions.php .gitignore

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: DWP-Functions
+Plugin URI: https://github.com/shoheiworks/wp-plugin-dwp-functions.git
+Description: Functions plugin
+Author: DWP-shohei
+Author URI: http://designseisaku.com
+Version: 0.1.1
+*/
